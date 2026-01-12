bb7b508ddd1c08f410791af0d1d84f0ccc30dd6d
mojhehh <sebmojheh@gmail.com>
2026-01-11 16:47:01 -0800

    chore: clean up branch - keep only script.js, style.css, index.html, and css/

--- js/main.js
+++
@@ -1,8 +0,0 @@
-// Main entry point for Chatra Chat App
-// This file imports all modules and initializes the app
-
-// For now, we import the original script.js to maintain compatibility
-// This will be refactored incrementally
-import '../script.js';
-
-console.log('[Chatra] Module system initialized');
